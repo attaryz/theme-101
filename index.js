8c4273d82350d1cf03fc61b1e914415e13aeffee
attaryz <markosberg123@gmail.com>
2022-08-19 19:10:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const test = () => {
+  console.log("test")
+}
+
+const xiketic = "#0D0106"
+const red_ryb = "#FF331F"
+const orange_red = "#F26419"
+const oxford_blue = "#02182B"
+const dodger_blue = "#0197F6"
+const middle_blue = "#68C5DB"
+const deep_pink = "#EC368D"
+const ksu_purple = "#5D2E8C"
+const tiffany_blue = "#2EC4B6"
+const cotton_candy = "#FFB8DE"
+const space_cadet = "#2D3142"
